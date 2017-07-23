de5f9e425a34ef247636c57e80f2491a3845e160
Gunnar Már Óttarsson <gunni@starcraft.is>
2017-07-23 16:29:17 +0000

    Initial

---
+++ CheckDebugFlags/CheckDebugFlags/AssemblyFlags.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+
+namespace CheckDebugFlags
+{
+    class AssemblyFlags
+    {
+        public AssemblyFlags(Assembly assembly)
+        {
+            Assembly = assembly;
+            LoadInformation();
+        }
+
+        public Assembly Assembly { get; private set; }
+
+        public bool EditAndContinueEnabled { get; private set; }
+
+        public bool JitOptimized { get; private set; }
+
+        public bool JitTrackingEnabled { get; private set; }
+
+        private void LoadInformation()
+        {
+            DebuggableAttribute debugAttribute = Assembly.GetCustomAttributes<DebuggableAttribute>().FirstOrDefault();
+
+            if (debugAttribute != null)
+            {
+                JitTrackingEnabled = debugAttribute.IsJITTrackingEnabled;
+                JitOptimized = !debugAttribute.IsJITOptimizerDisabled;
+                EditAndContinueEnabled = (debugAttribute.DebuggingFlags & DebuggableAttribute.DebuggingModes.EnableEditAndContinue) != DebuggableAttribute.DebuggingModes.None;
+            }
+            else
+            {
+                // No DebuggableAttribute means IsJITTrackingEnabled=false, IsJITOptimizerDisabled=false, EnableEditAndContinue=false
+                JitTrackingEnabled = false;
+                JitOptimized = true;
+                EditAndContinueEnabled = false;
+            }
+        }
+    }
+}
---
+++ CheckDebugFlags/CheckDebugFlags/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Reflection;
+using System.IO;
+
+namespace CheckDebugFlags
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            try
+            {
+                string path;
+
+                if (args.Length > 1)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                if (args.Length == 1)
+                {
+                    path = args[0];
+                }
+                else
+                {
+                    path = ".";
+                }
+
+                var curDirFiles = Directory.EnumerateFiles(path, "*.dll", SearchOption.AllDirectories);
+
+                foreach (var file in curDirFiles)
+                {
+                    string assemblyFullName = Path.GetFullPath(file);
+
+                    try
+                    {
+                        Assembly assembly = Assembly.LoadFile(assemblyFullName);
+                        var assemblyInformation = new AssemblyFlags(assembly);
+
+                        if (assemblyInformation.JitTrackingEnabled
+                        && !assemblyInformation.JitOptimized
+                        && assemblyInformation.EditAndContinueEnabled)
+                        {
+                            Console.WriteLine(assemblyFullName + " Debug");
+                        }
+                    }
+                    catch { }
+                }
+
+                return 0;
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine(
+                    @"CheckDebugFlags only accepts a single optional parameter. 
+                        The directory to start check from.
+                        When executed without parameters, the current directory is used."
+                );
+
+                return 1;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(
+                    "CheckDebugFlags hit an unhandled exception.."
+                );
+
+                return 2;
+            }
+        }
+    }
+}
